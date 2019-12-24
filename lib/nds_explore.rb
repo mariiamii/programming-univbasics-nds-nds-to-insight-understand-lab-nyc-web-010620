e4d23760c441841189ed605be95db7f82aa4b888
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-24 04:10:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,5 +10,9 @@ end
 
 def print_first_directors_movie_titles
   movies = directors_database[0][:movies] 
-  
+  i = 0
+  while i < movies.length do
+    puts movies[i][:title]
+    i += 1
+  end
 end
